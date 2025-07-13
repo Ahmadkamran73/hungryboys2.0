5a22dacdce8010b0edfb21d2f12328a0f2b1d9f5
Ahmad Kamran <f223822@cfd.nu.edu.pk>
2025-07-13 20:02:31 +0500

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'https://www.hungryboys.live',
+  'https://hungryboys2-0.vercel.app',
 ];
 
 const corsOptions = {
