1667d60f9324321409461c2d8f49a1b284527fcb
Ahmad Kamran <f223822@cfd.nu.edu.pk>
2025-07-13 19:36:30 +0500

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/hungry_boys112"
+  base: "/" // Remove the base path for Vercel deployment
 })
